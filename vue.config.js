429f14ccd942ae39389a337605e79f821caa0c51
Rachelbaa <rach.fbb@gmailcom>
2023-09-14 04:58:14 +0300

    changed config files

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Rachel-Furman-14-09-2023/'
-    : '/'
+  publicPath: "/"
 }
